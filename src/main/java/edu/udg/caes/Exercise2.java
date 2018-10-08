965f39f9b724d36e613d53f6d0075d94a869485f
BenjiRS94 <36006206+BenjiRS94@users.noreply.github.com>
2018-10-08 18:34:36 +0200

    Concrete union and your test

--- src/main/java/edu/udg/caes/Exercise2.java
+++ src/main/java/edu/udg/caes/Exercise2.java
@@ -11,6 +11,9 @@ public class Exercise2 {
      *  @param a first vector
      *  @param b second vector
      *  @return objects that are in either one of the two argument vectors
+     * @ Cal que cap dels Vectors que rep com a parametre siguin null
+     *   Si hi ha repetits els ajuntarà igualment
+     *   Si son dos vectors buits retornarà un vector buit
      */
     public static Vector union (Vector a, Vector b)
     {
--- src/test/java/edu/udg/caes/Exercise2Test.java
+++ src/test/java/edu/udg/caes/Exercise2Test.java
@@ -1,10 +1,83 @@
 package edu.udg.caes;
 
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Vector;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Exercise2Test {
 
-    // Tests not implemented yet
+    @Test
+    public void test_union_emptyVectors() {
+        Vector v1 = new Vector();
+        Vector v2 = new Vector();
+
+        Vector result = Exercise2.union(v1, v2);
+
+        assertEquals(result, new Vector());
+    }
+
+    @Test
+    public void test_union_notNullVectors() {
+        Vector v1 = null;
+        Vector v2 = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            Exercise2.union(v1, v2);
+        });
+    }
+
+    @Test
+    public void test_union_addSameElements() {
+
+        Vector v1 = new Vector();
+        v1.add(1);
+        Vector v2 = new Vector();
+        v2.add(1);
+        Vector expected = new Vector();
+        expected.add(1);
+        expected.add(1);
+
+        Vector result = Exercise2.union(v1, v2);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void test_union_addElements() {
+
+        Vector v1 = new Vector();
+        v1.add(1);
+        Vector v2 = new Vector();
+        v2.add(2);
+        Vector expected = new Vector();
+        expected.add(1);
+        expected.add(2);
+
+        Vector result = Exercise2.union(v1, v2);
+
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void test_union_acceptDiferentType() {
+
+        Vector v1 = new Vector();
+        v1.add(1);
+        Vector v2 = new Vector();
+        v2.add("halo");
+        Vector expected = new Vector();
+        expected.add(1);
+        expected.add("halo");
+
+        Vector result = Exercise2.union(v1, v2);
+
+        assertEquals(result, expected);
+    }
+
+
+
 
 }
